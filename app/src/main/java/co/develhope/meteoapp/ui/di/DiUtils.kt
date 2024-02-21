6a4ad17a22668be1fc2ef535c5f9337e371c2662
pola george <86931053+poula22@users.noreply.github.com>
2024-02-21 20:56:15 +0200

    Add dependency injection: Retrofit Annotation classes and searchScreen dependencies. SearchScreenFragment: move SearchRepo to TodayScreenViewModel

---
+++ app/src/main/java/co/develhope/meteoapp/ui/di/DiUtils.kt
@@ -0,0 +1,11 @@
+package co.develhope.meteoapp.ui.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WeatherRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class SearchRetrofit
--- app/src/main/java/co/develhope/meteoapp/ui/di/RepoModule.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/di/RepoModule.kt
@@ -1,5 +1,7 @@
 package co.develhope.meteoapp.ui.di
 
+import co.develhope.meteoapp.network.SearchRepo
+import co.develhope.meteoapp.network.SearchService
 import co.develhope.meteoapp.network.WeatherRepo
 import co.develhope.meteoapp.network.WeatherService
 import dagger.Module
@@ -13,5 +15,9 @@ object RepoModule {
     @Provides
     fun provideRepo(weatherService: WeatherService): WeatherRepo {
         return WeatherRepo(weatherService)
+    }
+    @Provides
+    fun provideSearchRepo(searchService: SearchService): SearchRepo {
+        return SearchRepo(searchService)
     }
 }
